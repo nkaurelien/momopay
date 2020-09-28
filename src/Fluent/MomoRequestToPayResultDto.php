d820bb8ae6e22ee9212b8cecfd838e51ce865099
nkaurelien <nkaurelien@gmail.com>
2020-09-28 19:03:59 +0100

    Refac!

--- src/Fluent/MomoRequestToPayResultDto.php
+++ src/Fluent/MomoRequestToPayResultDto.php
@@ -4,33 +4,31 @@
 namespace  Nkaurelien\Momopay\Fluent;
 
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Fluent;
-use Nkaurelien\Momopay\Exceptions\MomoPayException;
 use Nkaurelien\Momopay\Exceptions\PayerNotFoundException;
 use Nkaurelien\Momopay\Exceptions\ResourceNotFoundException;
 
 /**
  * Class MomoToken
  * @package  Nkaurelien\Momopay\Fluent
- * @property string amount
- * @property string payerMessage
- * @property string externalId
+ * @property-read string amount
+ * @property-read string payerMessage
+ * @property-read string externalId
  * @property string referenceId
- * @property string payeeNote
- * @property string currency default EUR
- * @property MomoPayerDto payer
- * @property string financialTransactionId
- * @property string status
- * @property string reason
- * @property string code
+ * @property-read string payeeNote
+ * @property-read string currency default EUR
+ * @property-read MomoPayerDto payer
+ * @property-read string financialTransactionId
+ * @property-read string status
+ * @property-read string reason
+ * @property-read string code
  */
 class MomoRequestToPayResultDto extends Fluent
 {
 
-    const  STATUT_SUCCESSFUL = 'SUCCESSFUL';
-    const  STATUT_FAILED = 'FAILED';
-    const  STATUT_PENDING = 'PENDING';
+    const  STATUS_SUCCESSFUL = 'SUCCESSFUL';
+    const  STATUS_FAILED = 'FAILED';
+    const  STATUS_PENDING = 'PENDING';
 
     const  FAILURE_PAYER_NOT_FOUND = 'PAYER_NOT_FOUND';
     const  FAILURE_RESOURCE_NOT_FOUND = 'RESOURCE_NOT_FOUND';
@@ -45,19 +43,24 @@ class MomoRequestToPayResultDto extends Fluent
         parent::__construct($attributes);
     }
 
+    public function isFailed()
+    {
+        return $this->status === self::STATUS_FAILED;
+    }
+
     public function isSuccessful()
     {
-        return $this->status === self::STATUT_SUCCESSFUL;
+        return $this->status === self::STATUS_SUCCESSFUL;
     }
 
     public function isPending()
     {
-        return $this->status === self::STATUT_PENDING;
+        return $this->status === self::STATUS_PENDING;
     }
 
-    public function isFailed()
+    public function isAccepted()
     {
-        return $this->status === self::STATUT_FAILED;
+        return $this->isPending();
     }
 
     public function isFailureBecausePayerNotFound()
@@ -81,6 +84,4 @@ class MomoRequestToPayResultDto extends Fluent
     {
         return self::$errorsMessages[$key] ?? 'error';
     }
-
-
 }
